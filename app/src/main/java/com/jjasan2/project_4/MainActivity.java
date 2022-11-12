148dc5bdb8d73aee2077d2f9a13aacc266808d4e
Johnit Jasan <jjohnit@gmail.com>
2022-11-12 14:35:33 -0600

    Modified the offensive strategy when one piece is in board

--- app/src/main/java/com/jjasan2/project_4/MainActivity.java
+++ app/src/main/java/com/jjasan2/project_4/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             public void handleMessage(@NonNull Message msg) {
                 // When player 1 completes a move, update the UI, check for win condition,
                 // run the next player
+                movesCounter++;
                 mHandler.post(new UpdateBoard());
                 // Stops the game when one player wins the game or there is tie
                 // Tie if the no one wins after 12 moves.
@@ -142,8 +143,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            movesCounter++;
-
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
@@ -166,69 +165,130 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            // When there are no pieces in board, add the piece to the first available space.
-            if(piecesInBoard == 0){
-                boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
-                msg = mHandler.obtainMessage(player);
-                mHandler.sendMessage(msg);
-                return;
-            }
-            // When there is 1 piece in the table, add the piece to an adjacent position
-            else if(piecesInBoard == 1){
-                // Check the adjacent row for free position
-                for (int i = 0; i < 3; i++) {
-                    if (boardStatus[existingPieces.get(0)[0]][i] == 0){
-                        boardStatus[existingPieces.get(0)[0]][i] = player;
-                        msg = mHandler.obtainMessage(player);
-                        mHandler.sendMessage(msg);
-                        return;
-                    }
-                }
-                // Check the adjacent column for free position
-                for (int i = 0; i < 3; i++) {
-                    if (boardStatus[i][existingPieces.get(0)[1]] == 0){
-                        boardStatus[i][existingPieces.get(0)[1]] = player;
-                        msg = mHandler.obtainMessage(player);
-                        mHandler.sendMessage(msg);
-                        return;
-                    }
-                }
-            }
-            else if(piecesInBoard == 2){
-                // Check for possible win conditions
-                boolean pieceAdded = checkWinConditions(existingPieces.get(0), existingPieces.get(1));
-                // If the existing pieces cannot be utilized for win condition,
-                // add the piece in an available position
-                if (!pieceAdded)
+            // Only one thread should modify the board at a time.
+            synchronized (boardStatus){
+                // When there are no pieces in board, add the piece to the first available space.
+                if(piecesInBoard == 0){
                     boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
-                msg = mHandler.obtainMessage(player);
-                mHandler.sendMessage(msg);
-                return;
-            }
-            // When there's 3 pieces in the board, replace one
-            else{
-                for (int i = 0; i < 2; i++) {
-                    for (int j = i + 1; j < 3; j++) {
-                        boolean pieceAdded = checkWinConditions(existingPieces.get(i), existingPieces.get(j));
-                        // if win condition found, remove the extra piece from board
-                        if (pieceAdded){
-                            List<Integer[]> positionToRemove = new ArrayList<>(existingPieces);
-                            positionToRemove.remove(existingPieces.get(i));
-                            positionToRemove.remove(existingPieces.get(j));
-                            boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
-                            msg = mHandler.obtainMessage(player);
+                    msg = mHandler.obtainMessage(player);
+                    mHandler.sendMessage(msg);
+                    return;
+                }
+                // When there is 1 piece in the table, add the piece to an adjacent position
+                else if(piecesInBoard == 1){
+                    boolean boardUpdatedFlag = false;
+                    // Check the adjacent row for free position
+//                    for (int i = 0; i < 3; i++) {
+//                        if (boardStatus[existingPieces.get(0)[0]][i] == 0){
+//                            boardStatus[existingPieces.get(0)[0]][i] = player;
+//                            msg = mHandler.obtainMessage(player);
+//                            mHandler.sendMessage(msg);
+//                            return;
+//                        }
+//                    }
+
+                    switch (existingPieces.get(0)[0]){
+                        case 0:
+                        case 2:
+                            if (boardStatus[existingPieces.get(0)[0]][1] == 0){
+                                boardStatus[existingPieces.get(0)[0]][1] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            break;
+                        case 1:
+                            if (boardStatus[existingPieces.get(0)[0]][0] == 0){
+                                boardStatus[existingPieces.get(0)[0]][0] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            else if (boardStatus[existingPieces.get(0)[0]][2] == 0){
+                                boardStatus[existingPieces.get(0)[0]][2] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            break;
+                    }
+
+                    if (boardUpdatedFlag){
+                        msg = mHandler.obtainMessage(player);
                             mHandler.sendMessage(msg);
                             return;
+                    }
+
+                    // Check the adjacent column for free position
+                    switch (existingPieces.get(0)[1]) {
+                        case 0:
+                        case 2:
+                            if (boardStatus[1][existingPieces.get(0)[1]] == 0) {
+                                boardStatus[1][existingPieces.get(0)[1]] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            break;
+                        case 1:
+                            if (boardStatus[0][existingPieces.get(0)[1]] == 0) {
+                                boardStatus[0][existingPieces.get(0)[1]] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            else if (boardStatus[2][existingPieces.get(0)[1]] == 0) {
+                                boardStatus[2][existingPieces.get(0)[1]] = player;
+                                boardUpdatedFlag = true;
+                            }
+                            break;
+                    }
+//                    for (int i = 0; i < 3; i++) {
+//                        if (boardStatus[i][existingPieces.get(0)[1]] == 0){
+//                            boardStatus[i][existingPieces.get(0)[1]] = player;
+//                            msg = mHandler.obtainMessage(player);
+//                            mHandler.sendMessage(msg);
+//                            return;
+//                        }
+//                    }
+                    if (boardUpdatedFlag){
+                        msg = mHandler.obtainMessage(player);
+                        mHandler.sendMessage(msg);
+                        return;
+                    }
+
+                    // If unable to find an adjacent position, use any free position
+                    boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
+                    msg = mHandler.obtainMessage(player);
+                    mHandler.sendMessage(msg);
+                    return;
+                }
+                else if(piecesInBoard == 2){
+                    // Check for possible win conditions
+                    boolean pieceAdded = checkWinConditions(existingPieces.get(0), existingPieces.get(1));
+                    // If the existing pieces cannot be utilized for win condition,
+                    // add the piece in an available position
+                    if (!pieceAdded)
+                        boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
+                    msg = mHandler.obtainMessage(player);
+                    mHandler.sendMessage(msg);
+                    return;
+                }
+                // When there's 3 pieces in the board, replace one
+                else{
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = i + 1; j < 3; j++) {
+                            boolean pieceAdded = checkWinConditions(existingPieces.get(i), existingPieces.get(j));
+                            // if win condition found, remove the extra piece from board
+                            if (pieceAdded){
+                                List<Integer[]> positionToRemove = new ArrayList<>(existingPieces);
+                                positionToRemove.remove(existingPieces.get(i));
+                                positionToRemove.remove(existingPieces.get(j));
+                                boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
+                                msg = mHandler.obtainMessage(player);
+                                mHandler.sendMessage(msg);
+                                return;
+                            }
                         }
                     }
+                    // If the existing pieces cannot be utilized for win condition,
+                    // add the piece in an available position and remove an existing piece
+                    boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
+                    boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
+                    msg = mHandler.obtainMessage(player);
+                    mHandler.sendMessage(msg);
+                    return;
                 }
-                // If the existing pieces cannot be utilized for win condition,
-                // add the piece in an available position and remove an existing piece
-                boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
-                boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
-                msg = mHandler.obtainMessage(player);
-                mHandler.sendMessage(msg);
-                return;
             }
         }
 
@@ -265,8 +325,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            movesCounter++;
-
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
@@ -275,10 +333,14 @@ public class MainActivity extends AppCompatActivity {
             Random randomGenerator = new Random();
             int row = randomGenerator.nextInt(3);
             int col = randomGenerator.nextInt(3);
-            boardStatus[row][col] = player;
-            msg = mHandler.obtainMessage(player);
-            mHandler.sendMessage(msg);
-            return;
+
+            // Only one thread should modify the board at a time.
+            synchronized (boardStatus){
+                boardStatus[row][col] = player;
+                msg = mHandler.obtainMessage(player);
+                mHandler.sendMessage(msg);
+                return;
+            }
         }
     }
 
@@ -294,7 +356,7 @@ public class MainActivity extends AppCompatActivity {
                     + boardStatus[2][0] + " " + boardStatus[2][1] + " " + boardStatus[2][2]);
             // Check for win conditions
             if (isPlayerWon() || movesCounter > 12){
-                Log.i("appDebug", playerWon +" won");
+                Log.i("appDebug", "Player " + playerWon +" won");
             }
         }
     }
