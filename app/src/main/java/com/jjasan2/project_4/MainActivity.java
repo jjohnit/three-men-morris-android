00c0c75359e41475269f2d1acb45efd798af99fe
Johnit Jasan <jjohnit@gmail.com>
2022-11-12 05:18:55 -0600

    Working in log

--- app/src/main/java/com/jjasan2/project_4/MainActivity.java
+++ app/src/main/java/com/jjasan2/project_4/MainActivity.java
@@ -1,9 +1,11 @@
 package com.jjasan2.project_4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 
@@ -14,9 +16,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     Player1Thread player1;
     Player2Thread player2;
-    // To sequentially run the threads based on the player.
-    int nextPlayer = 1;
-    boolean gameOver = false;
+    Strategy p1Strategy, p2Strategy;
 
     // Status of the board. 0 means Empty, 1 means player 1, 2 means player 2
     public int[][] boardStatus = {{0,0,0},{0,0,0},{0,0,0}};
@@ -30,7 +30,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mHandler = new Handler(getMainLooper());
+        mHandler = new Handler(getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                // When player 1 completes a move, update the UI, check for win condition,
+                // run the next player
+                mHandler.post(new UpdateBoard());
+                if(isPlayerWon() || movesCounter > 12){
+                    // End game and display message
+                    Log.i("appDebug", "Player " + msg.what + " won");
+                    player1.interrupt();
+                    player2.interrupt();
+                    player1 = new Player1Thread();
+                    player2 = new Player2Thread();
+                    player1.start();
+                    player2.start();
+                    movesCounter = 0;
+                }
+                else if (msg.what == 1){
+                    player2.p2Handler.post((Runnable) p2Strategy);
+                }
+                else {
+                    player1.p1Handler.post((Runnable) p1Strategy);
+                }
+                return;
+            }
+        };
         player1 = new Player1Thread();
         player2 = new Player2Thread();
         player1.start();
@@ -59,45 +84,98 @@ public class MainActivity extends AppCompatActivity {
 
     // Method to start the game between the players
     protected void startGame(){
-        Handler firstPlayer, secondPlayer;
-        Random randomNumber = new Random();
+//        Handler firstPlayer, secondPlayer;
 
-        // Randomly selects the first player to start the game.
-        if(randomNumber.nextInt(2) == 0) {
-            firstPlayer = player1.p1Handler;
-            secondPlayer = player2.p2Handler;
-        }
-        else{
-            firstPlayer = player2.p2Handler;
-            secondPlayer = player1.p1Handler;
-        }
+        // Randomly selects the first player to start the game.\
+//        Random randomNumber = new Random();
+//        if(randomNumber.nextInt(2) == 0) {
+//            firstPlayer = player1.p1Handler;
+//            secondPlayer = player2.p2Handler;
+//        }
+//        else{
+//            firstPlayer = player2.p2Handler;
+//            secondPlayer = player1.p1Handler;
+//        }
 
-        while (!gameOver && movesCounter <= 12) {
-            nextPlayer = 1;
-            firstPlayer.post(new StrategyOffensive(1));
-            nextPlayer = 2;
-            secondPlayer.post(new StrategyOffensive(2));
-            movesCounter++;
-        }
-        gameOver = false;
+//        while (!gameOver && movesCounter <= 12) {
+//            firstPlayer.post(new StrategyOffensive(1));
+//            secondPlayer.post(new StrategyOffensive(2));
+//            movesCounter++;
+//        }
+//        gameOver = false;
+
         movesCounter = 0;
+        // Randomly selects the strategy to be used by the players
+        Random randomGenerator = new Random();
+        if (randomGenerator.nextInt(2) == 0){
+            p1Strategy = new StrategyOffensive(1);
+            Log.i("appDebug", "Player 1 uses strategy - Offensive");
+        }
+        else {
+            p1Strategy = new StrategyRandom(1);
+            Log.i("appDebug", "Player 1 uses strategy - Random");
+        }
+        if (randomGenerator.nextInt(2) == 0){
+            p2Strategy = new StrategyOffensive(2);
+            Log.i("appDebug", "Player 2 uses strategy - Offensive");
+        }
+        else {
+            p2Strategy = new StrategyRandom(2);
+            Log.i("appDebug", "Player 2 uses strategy - Random");
+        }
+//        p1Strategy = new StrategyRandom(1);
+//        p2Strategy = new StrategyRandom(2);
+        // Starts the game by calling the first player
+        player1.p1Handler.post((Runnable) p1Strategy);
+    }
+
+    // To check whether any player won
+    public boolean isPlayerWon(){
+        // Check whether any complete row have same value
+        for (int i = 0; i < 3; i++) {
+            if((boardStatus[i][0] > 0) && (boardStatus[i][0] == boardStatus[i][1])
+                    && (boardStatus[i][1] == boardStatus[i][2])){
+                return true;
+            }
+        }
+        // Check whether any complete column have same value
+        for (int i = 0; i < 3; i++) {
+            if((boardStatus[0][i] > 0) && (boardStatus[0][i] == boardStatus[1][i])
+                    && (boardStatus[1][i] == boardStatus[2][i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Base class for strategies
+    public class Strategy {
+
+        int player;
+        Message msg = mHandler.obtainMessage(player);
+
+        public Strategy(int player) {
+            this.player = player;
+            msg.what = player;
+        }
     }
 
     // Strategy focussed on victory
-    public class StrategyOffensive implements Runnable {
-
-        int player;
+    public class StrategyOffensive extends Strategy implements Runnable {
 
         public StrategyOffensive(int player) {
-            this.player = player;
+            super(player);
         }
 
         @Override
         public void run() {
-            Log.i("appDebug", "Offensive strategy by player " + player);
 
-            while (player != nextPlayer){
-                // Wait for the players chance
+            movesCounter++;
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
             int piecesInBoard = 0;
@@ -119,6 +197,9 @@ public class MainActivity extends AppCompatActivity {
             // When there are no pieces in board, add the piece to the first available space.
             if(piecesInBoard == 0){
                 boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
+                msg = mHandler.obtainMessage(player);
+                mHandler.sendMessage(msg);
+                return;
             }
             // When there is 1 piece in the table, add the piece to an adjacent position
             else if(piecesInBoard == 1){
@@ -126,7 +207,8 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < 3; i++) {
                     if (boardStatus[existingPieces.get(0)[0]][i] == 0){
                         boardStatus[existingPieces.get(0)[0]][i] = player;
-                        mHandler.post(new UpdateBoard(player));
+                        msg = mHandler.obtainMessage(player);
+                        mHandler.sendMessage(msg);
                         return;
                     }
                 }
@@ -134,7 +216,8 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < 3; i++) {
                     if (boardStatus[i][existingPieces.get(0)[1]] == 0){
                         boardStatus[i][existingPieces.get(0)[1]] = player;
-                        mHandler.post(new UpdateBoard(player));
+                        msg = mHandler.obtainMessage(player);
+                        mHandler.sendMessage(msg);
                         return;
                     }
                 }
@@ -146,7 +229,8 @@ public class MainActivity extends AppCompatActivity {
                 // add the piece in an available position
                 if (!pieceAdded)
                     boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
-                mHandler.post(new UpdateBoard(player));
+                msg = mHandler.obtainMessage(player);
+                mHandler.sendMessage(msg);
                 return;
             }
             // When there's 3 pieces in the board, replace one
@@ -160,7 +244,8 @@ public class MainActivity extends AppCompatActivity {
                             positionToRemove.remove(existingPieces.get(i));
                             positionToRemove.remove(existingPieces.get(j));
                             boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
-                            mHandler.post(new UpdateBoard(player));
+                            msg = mHandler.obtainMessage(player);
+                            mHandler.sendMessage(msg);
                             return;
                         }
                     }
@@ -169,7 +254,8 @@ public class MainActivity extends AppCompatActivity {
                 // add the piece in an available position and remove an existing piece
                 boardStatus[possibleMoves.get(0)[0]][possibleMoves.get(0)[1]] = player;
                 boardStatus[existingPieces.get(0)[0]][existingPieces.get(0)[1]] = 0;
-                mHandler.post(new UpdateBoard(player));
+                msg = mHandler.obtainMessage(player);
+                mHandler.sendMessage(msg);
                 return;
             }
         }
@@ -198,32 +284,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Strategy focussed on defending/preventing the other player from winning
-    public class StrategyDefensive implements Runnable {
+    public class StrategyRandom extends Strategy implements Runnable {
 
-        int player;
-
-        public StrategyDefensive(int player) {
-            this.player = player;
+        public StrategyRandom(int player) {
+            super(player);
         }
 
         @Override
         public void run() {
-            Log.i("appDebug", "Defensive strategy by player " + player);
+
+            movesCounter++;
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Random randomGenerator = new Random();
+            int row = randomGenerator.nextInt(3);
+            int col = randomGenerator.nextInt(3);
+            boardStatus[row][col] = player;
+            msg = mHandler.obtainMessage(player);
+            mHandler.sendMessage(msg);
+            return;
         }
     }
 
     // To update the board in the UI
     public class UpdateBoard implements Runnable {
-        int player;
-        public UpdateBoard(int player) {
-            this.player = player;
-        }
 
         @Override
         public void run() {
             // Check for win conditions
             // Update the UI
-            Log.i("appDebug", "Player " + player + " updated board to ");
+            Log.i("appDebug", "Move " + movesCounter +" Board : ");
             Log.i("appDebug", boardStatus[0][0] + " " + boardStatus[0][1] + " " + boardStatus[0][2] + " "
                     + boardStatus[1][0] + " " + boardStatus[1][1] + " " + boardStatus[1][2] + " "
                     + boardStatus[2][0] + " " + boardStatus[2][1] + " " + boardStatus[2][2]);
